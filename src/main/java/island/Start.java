b4f8dd939625042f576398d1a58b017ba7858267
Aleksey27007 <alekseyvaruj@gmail.com>
2025-02-05 21:05:07 +0300

    Island added few func

--- src/main/java/island/Start.java
+++ src/main/java/island/Start.java
@@ -3,15 +3,24 @@ package island;
 import island.animals.Animal;
 import island.animals.herbivores.Goat;
 import island.animals.carnivores.Wolf;
+import island.island.Island;
+
+import java.util.ArrayList;
+import java.util.Arrays;
 
 
 public class Start {
     public static void main(String[] args) {
-        Animal goat = new Goat();
-        Animal wolf = new Wolf();
+        Island island = new Island("100x100");
+        System.out.println(island.getSize());
 
-        goat.eat();
-        wolf.eat();
+        int[][] array = island.getCoordinates();
 
+        for (int i = 0; i < array.length; i++) {
+            for (int j = 0; j < array[i].length; j++) {
+                System.out.printf("%d" + " ", array[i][j]);
+            }
+            System.out.println();
+        }
     }
 }
--- src/main/java/island/island/Island.java
+++ src/main/java/island/island/Island.java
@@ -1,15 +1,50 @@
 package island.island;
 
-import lombok.AllArgsConstructor;
 import lombok.Data;
 
+import java.util.Arrays;
 import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
-@AllArgsConstructor
 @Data
 public class Island {
-    private static String size;
-    private static Cage cage; // 1 ячейка поля
-    private static int maxValueOfAnimals;
-    private List[][] coordinates;
+    private String size;
+    private List<Cage> cage; // 1 ячейка поля
+    private int valueOfAnimals;
+    // [] строки [] столбцы
+    private int[][] coordinates;
+    private int x;
+    private int y;
+
+
+    public Island(String size) {
+        this.size = size;
+        checkSizeFormat(size);
+        this.coordinates = fillIslandCoord();
+    }
+
+    private static void checkSizeFormat(String islandSize) {
+
+        Pattern pattern = Pattern.compile("[0-9]{3}x{1}[0-9]{3}");
+        Matcher matcher = pattern.matcher(islandSize);
+
+        if (!matcher.matches()) {
+            throw new IllegalArgumentException("size argument is bad need format 000x000 for example");
+        }
+    }
+
+    private int[][] fillIslandCoord() {
+        islandSizeToInt(size);
+
+        return new int[x][y];
+    }
+
+    private void islandSizeToInt(String size) {
+        List<String> list = Arrays.stream(size.split("x")).toList();
+        if (x == 0 && y == 0) {
+            x = Integer.parseInt(list.get(0));
+            y = Integer.parseInt(list.get(1));
+        }
+    }
 }
